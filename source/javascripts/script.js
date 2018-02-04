e2f7c073b382380da4921cdb57b202fb8eaba7ea
Michał Rakowski <michal.rakowski@dojomadness.com>
2018-02-04 19:05:07 +0100

    work work

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -15,10 +15,6 @@ function highlightButtonOnDropdownActive() {
   });
 }
 
-$(document).ready(function(){
-  highlightButtonOnDropdownActive();
-});
-
 $(document).ready(function() {
   $('.main').addClass('fadeIn');
 });
@@ -28,6 +24,62 @@ $(document).ready(function(){
         $(".mobileNav__nav").slideToggle();
     });
 });
+
+function highlightNavigationLinks() {
+
+  $('.header__nav-item a').on('click', function() {
+    var navigationLinks = $('.header__nav-item');
+    navigationLinks.each(function(index, item) {
+      $(item).removeClass('header__nav-item--active');
+    })
+
+    $(this).parent().addClass('header__nav-item--active');
+  })
+
+  $(document).on('scroll', function() {
+    var navigationLinks = $('.header__nav-item a');
+    var navigationElementHeight = $('.header__nav').height();
+    var fromTop = $(this).scrollTop()
+    var currentSection;
+
+    $('.section').each(function(){
+      var sectionOffset = $(this).offset().top;
+
+      if (sectionOffset - 1 < fromTop + navigationElementHeight) {
+        currentSection = $(this)
+      }
+
+      function isLastSection() {
+        return ($(window).scrollTop() >= $(document).height() - $(window).height())
+      }
+
+      var id = isLastSection() ? 'contact' : currentSection.attr('id');
+
+      navigationLinks.each(function(index, item) {
+        $(item).parent().removeClass('header__nav-item--active');
+      })
+
+      navigationLinks.each(function(index, item) {
+        if ($(this).data('scroll-to') === id) {
+          $(item).parent().addClass('header__nav-item--active');
+        }
+      })
+    })
+  })
+}
+
+function animateScrollToSection() {
+  var callback = function() {
+    console.log('koniec animacji')
+  }
+
+  $('.header__nav-item a').on('click', function() {
+    var navigationBarHeight = $('.header__nav').height();
+    var scrollOffset = $('#' + this.dataset.scrollTo).offset().top - navigationBarHeight;
+
+    $('body, html').animate({'scrollTop': scrollOffset}, 500, 'swing', callback);
+  })
+}
 
 function initMap() {
   var uluru = {lat: 53.4387859, lng: 14.562607299999968};
@@ -40,3 +92,45 @@ function initMap() {
     map: map
   });
 }
+
+function getDataFromAPI() {
+  $('#dataButton').on('click', function(){
+
+    $(this).text('loading...').css({'opacity': '0.5', 'pointer-events': 'none'})
+
+    $.ajax({
+      url: 'http://5a74c66b08118e0012fd4c9a.mockapi.io/raku/cars',
+      data: {},
+      type: 'GET',
+      success: function(data) {
+        onRequestSuccess(data)
+        $('#dataButton').text('button').css({'opacity': '1', 'pointer-events': 'initial'})
+      },
+      error: function(JqXHR, textStatus, errorThrown) {
+        onRequestError(errorThrown);
+        $('#dataButton').text('button').css({'opacity': '1', 'pointer-events': 'initial'})
+      }
+    })
+  })
+}
+
+function onRequestError(error) {
+  alert(error)
+}
+
+function onRequestSuccess(data) {
+  $('#dataItems').html('');
+  $.each(data, function(index, car) {
+    var itemTemplate = '<div>Model: ' + car.model + '<br/>Engine: ' + car.engine + '<br/><br/></div>';
+    $('#dataItems').append(itemTemplate);
+  })
+}
+
+$(document).ready(function(){
+  highlightButtonOnDropdownActive();
+  animateScrollToSection();
+  highlightNavigationLinks();
+  getDataFromAPI();
+});
+
+
